01169af4ea73390f2c96ce242e22c7d13b91800c
aammeerraakhater <aa9828056@gmail.com>
2024-05-21 15:42:15 +0300

    some unsaved changes

---
+++ editCustomer.php
@@ -0,0 +1,29 @@
+<?php
+ob_start();
+session_start();
+include 'includes/header.php'      ?>
+<div class="container-scroller">
+    <!-- partial:partials/_navbar.html -->
+
+    <?php include 'init.php';
+    include 'nav.php' ?>
+    <!-- partial -->
+    <div class="container-fluid page-body-wrapper">
+        <!-- partial:partials/_sidebar.html -->
+        <?php
+        include 'sideNav.php';
+        ?>
+        <!-- partial -->
+        <div class="main-panel">
+            <div class="content-wrapper">
+            </div>
+            <!-- content-wrapper ends -->
+            <!-- partial:partials/_footer.html -->
+            <!-- partial -->
+        </div>
+        <!-- main-panel ends -->
+    </div>
+    <!-- page-body-wrapper ends -->
+</div>
+<?php include 'includes/footer.php';
+ob_end_flush();
